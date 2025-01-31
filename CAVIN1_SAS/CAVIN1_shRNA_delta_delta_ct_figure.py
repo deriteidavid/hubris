ba785ff05c96263796b2785b43c73dac9c6e3476
David Deritei <deriteidavid@gmail.com>
2025-01-31 10:14:32 -0500

    Update CAVIN1_shRNA_delta_delta_ct_figure.py
    added ** for p<=0.01

--- CAVIN1_SAS/CAVIN1_shRNA_delta_delta_ct_figure.py
+++ CAVIN1_SAS/CAVIN1_shRNA_delta_delta_ct_figure.py
@@ -8,11 +8,13 @@ plt.figure(figsize=(8,6))
 plt.bar(range(len(df)),df['Delta delta Ct (estimate)'],yerr=df['Std Error'])
 plt.xticks(range(len(df)),df['Gene'])
 for i,p_value in enumerate(df['P-value']):
-    if float(p_value) <=0.05:
+    if (float(p_value) <=0.05) and (float(p_value) >0.01):
         plt.text(x=i, y=df['Delta delta Ct (estimate)'][i]+df['Std Error'][i]+0.1, s="*", ha='center', va='center', fontsize=15)
+    if (float(p_value) <=0.01):
+        plt.text(x=i, y=df['Delta delta Ct (estimate)'][i]+df['Std Error'][i]+0.1, s="**", ha='center', va='center', fontsize=15)
 plt.ylabel('Delta Delta Ct')
 sns.despine()
 
-plt.savefig('CR_outputs/CAVIN1_delta_delta_ct.png',dpi=300)
-plt.savefig('CR_outputs/CAVIN1_delta_delta_ct.pdf')
+plt.savefig('CAVIN1_delta_delta_ct.png',dpi=600)
+plt.savefig('CAVIN1_delta_delta_ct.pdf')
 
